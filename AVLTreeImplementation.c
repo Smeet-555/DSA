fed0de1a9f1df831e0c5a4e75c240ff3650d984e
Smeet Raichura <smeetr2006@gmail.com>
2025-07-26 18:14:53 +0530

    removed the comments

--- AVLTreeImplementation.c
+++ AVLTreeImplementation.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Define the Node structure
 struct Node {
     int key;
     struct Node* left;
@@ -9,72 +8,59 @@ struct Node {
     int height;
 };
 
-// Helper function to get max of two integers
 int max(int a, int b) {
     return (a > b) ? a : b;
 }
 
-// Get height of a node
 int height(struct Node* N) {
     if (N == NULL)
         return 0;
     return N->height;
 }
 
-// Create a new node
 struct Node* createNode(int key) {
     struct Node* node = (struct Node*)malloc(sizeof(struct Node));
     node->key = key;
     node->left = NULL;
     node->right = NULL;
-    node->height = 1;  // new node is initially at leaf
+    node->height = 1;
     return node;
 }
 
-// Right rotate subtree rooted with y
 struct Node* rightRotate(struct Node* y) {
     struct Node* x = y->left;
     struct Node* T2 = x->right;
 
-    // Perform rotation
     x->right = y;
     y->left = T2;
 
-    // Update heights
     y->height = max(height(y->left), height(y->right)) + 1;
     x->height = max(height(x->left), height(x->right)) + 1;
 
-    // Return new root
     return x;
 }
 
-// Left rotate subtree rooted with x
 struct Node* leftRotate(struct Node* x) {
     struct Node* y = x->right;
     struct Node* T2 = y->left;
 
-    // Perform rotation
     y->left = x;
     x->right = T2;
 
-    // Update heights
+
     x->height = max(height(x->left), height(x->right)) + 1;
     y->height = max(height(y->left), height(y->right)) + 1;
 
-    // Return new root
     return y;
 }
 
-// Get balance factor
 int getBalance(struct Node* N) {
     if (N == NULL)
         return 0;
     return height(N->left) - height(N->right);
 }
 
-// Insert a key into the AVL tree
 struct Node* insert(struct Node* node, int key) {
-    // 1. Perform the normal BST insertion
     if (node == NULL)
         return createNode(key);
 
@@ -82,42 +68,34 @@ struct Node* insert(struct Node* node, int key) {
         node->left = insert(node->left, key);
     else if (key > node->key)
         node->right = insert(node->right, key);
-    else // Equal keys not allowed
+    else 
         return node;
 
-    // 2. Update height of this ancestor node
     node->height = 1 + max(height(node->left), height(node->right));
 
-    // 3. Get the balance factor
     int balance = getBalance(node);
 
-    // 4. If unbalanced, then do the appropriate rotation
 
-    // Left Left Case
+
     if (balance > 1 && key < node->left->key)
         return rightRotate(node);
 
-    // Right Right Case
     if (balance < -1 && key > node->right->key)
         return leftRotate(node);
 
-    // Left Right Case
     if (balance > 1 && key > node->left->key) {
         node->left = leftRotate(node->left);
         return rightRotate(node);
     }
 
-    // Right Left Case
     if (balance < -1 && key < node->right->key) {
         node->right = rightRotate(node->right);
         return leftRotate(node);
     }
 
-    // Return the unchanged node pointer
     return node;
 }
 
-// In-order traversal
 void inOrder(struct Node* root) {
     if (root != NULL) {
         inOrder(root->left);
@@ -126,7 +104,6 @@ void inOrder(struct Node* root) {
     }
 }
 
-// Main function to test the AVL tree
 int main() {
     struct Node* root = NULL;
     int choice = 1 , key;
@@ -145,7 +122,6 @@ int main() {
 
 
 
-    // Print in-order traversal
     printf("Inorder traversal of the AVL tree:\n");
     inOrder(root);
 
