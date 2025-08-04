b1b45ae8ed2ce76bbbb8c2f60ba7b4c6828057a7
smeet raichura <smeetr2006@gmail.com>
2025-08-04 20:48:24 +0530

    enqueue and dequeue

---
+++ Queue.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+int r = 0, f = 0;
+void enque(int val , int size , int queue[]){
+
+    if (r >= size)
+    {
+        printf("Queue overflow");
+    }
+    queue[r++] = val;        
+}
+
+
+void dequeue(int size , int queue[]){
+    if (r == f)
+    {
+        printf("queue overflow");
+    }
+    f++;
+}
+void display(int queue[] , int size){
+    for (int i = f; i < r; i++)
+    {
+        printf("%d " , queue[i]);
+    }   
+}
+int main(){
+    int size;
+    printf("Enter the size of queue: ");
+    scanf("%d" , &size);
+    int queue[size];
+    enque(5 , size , queue);
+    enque(10 , size , queue);
+    enque(15, size , queue);
+    enque(20 , size , queue);
+    
+    display(queue , size);
+    
+    dequeue(size , queue);
+    dequeue(size , queue);
+    printf("\n");
+    display(queue , size);
+
+}
